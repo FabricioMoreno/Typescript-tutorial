b00aaed082294968117d744985da97a23232dbd0
Fabricio Moreno <fabricio.morenonaranjo@hotmail.com>
2023-03-08 14:50:32 -0500

    Primitives types

---
+++ intro/primatives.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var word = "here";
+var word1 = "here"; //autodetect type without explicit assignment
+var randomNumber = 32.234; //Just exits number not float, etc
+randomNumber.toFixed();
+var status = true; //boolean
---
+++ intro/primatives.ts
@@ -0,0 +1,10 @@
+let word : string = "here"
+let word1 = "here" //autodetect type without explicit assignment
+
+let randomNumber = 32.234 //Just exits number not float, etc
+randomNumber.toFixed()
+
+let status = true //boolean
+
+export {}
+
