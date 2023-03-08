ddf732436679014e97143101c7fb102e57f8cfd9
Fabricio Moreno <fabricio.morenonaranjo@hotmail.com>
2023-03-08 14:33:16 -0500

    Intro for ts

---
+++ intro/hello-world.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var input = "Hello world";
+console.log("My name is Fabricio and this is my ".concat(input));
---
+++ intro/hello-world.ts
@@ -0,0 +1,5 @@
+const input = "Hello world"
+
+console.log(`My name is Fabricio and this is my ${input}`)
+
+export {}
