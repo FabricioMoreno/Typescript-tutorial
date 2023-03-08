20831f27bf8497762e76da5bb5a9306cbdc88f40
Fabricio Moreno <fabricio.morenonaranjo@hotmail.com>
2023-03-08 14:51:17 -0500

    Use of any type

---
+++ intro/any-type.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+//Dont use any
+var word;
+function generateWord() {
+    return "new word";
+}
+word = generateWord(); // this word has any type
+//Solution: declaring word variable with type
+var wordd;
+function generateWordd() {
+    return "new word";
+}
+wordd = generateWordd(); // this word has any string type
---
+++ intro/any-type.ts
@@ -0,0 +1,19 @@
+//Dont use any
+let word
+
+function generateWord(){
+    return "new word"
+}
+
+word = generateWord() // this word has any type
+
+//Solution: declaring word variable with type
+let wordd : string
+
+function generateWordd(){
+    return "new word"
+}
+
+wordd = generateWordd() // this word has any string type
+
+export {}
