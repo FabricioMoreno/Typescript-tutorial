164b56d94847df7882c594eaef90c23f1770bd73
Fabricio Moreno <fabricio.morenonaranjo@hotmail.com>
2023-03-08 15:01:16 -0500

    function pt1

---
+++ intro/functions-pt1.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(345);
+var loginUser = function (name, email) {
+    console.log("login");
+};
+loginUser("fabricio", "exameple@email.con");
---
+++ intro/functions-pt1.ts
@@ -0,0 +1,15 @@
+function addTwo(num:number){
+    return num+2
+}
+
+addTwo(345) 
+
+const loginUser = (name:string,email:string) =>{
+    console.log("login")
+}
+
+loginUser("fabricio","exameple@email.con")
+
+
+
+export {}
